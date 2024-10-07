4563d4f05f45425096a25484c4a21d97a847aa61
Akila-PHP <143219353+akila199910@users.noreply.github.com>
2024-10-07 16:30:38 +0530

    create user schema

---
+++ backend/models/user.model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+import Ajv from "ajv";
+
+const ajv = new Ajv();
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        
+    },
+    contact:{
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        enum: ["admin", "user"],
+        default: "user",
+    },
+});
+
+export default mongoose.model("User", userSchema);
---
+++ backend/schema/user.schema.js
@@ -0,0 +1,41 @@
+const userSchema = {
+  type: "object",
+  properties: {
+    firstName: {
+      type: "string",
+      required: true,
+      minLength: 3,
+      maxLength: 30,
+      pattern: "^[a-zA-Z]+$", //only letters
+    },
+    lastName: {
+      type: "string",
+      required: true,
+      minLength: 3,
+      maxLength: 30,
+      pattern: "^[a-zA-Z]+$", //only letters
+    },
+    contact: {
+      type: "string",
+      pattern: "^[0-9]{10}$",
+      required: true,
+      length: 10,
+    },
+    email: {
+      type: "string",
+      required: true,
+      minLength: 3,
+      maxLength: 30,
+      format: "email",
+    },
+    role: {
+      type: "string",
+      required: true,
+      enum: ["super_admin", "admin", "business_user", "user", "guest"],
+    },
+    profilePic: {
+      type: "string",
+      required: false,
+    },
+  },
+};
